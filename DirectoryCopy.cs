6b438abb7b0538d276656fe04f8576bab9ea1949
WangJibo <w5571393#>
2021-02-11 22:16:41 +0800

    first commit

---
+++ DirectoryCopy.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+
+namespace CSharpConsoleDemo
+{
+    public class DirectoryCopy
+    {
+
+        public static bool Copy(string sourceDirName, string destDirName, out string error)
+        {
+            return Copy(sourceDirName, destDirName, out error, null, false);
+        }
+
+        /// <summary>
+        /// 
+        /// 替换文件
+        /// 
+        /// </summary>
+        /// <param name="sourceDirName"></param>
+        /// <param name="destDirName"></param>
+        /// <param name="error"></param>
+        /// <returns></returns>
+        public static bool Copy(string sourceDirName, string destDirName, out string error, ArrayList sqlScriptList, bool logger)
+        {
+            error = string.Empty;
+            string filename = "";
+            try
+            {
+                if (!Directory.Exists(destDirName))
+                {
+                    Directory.CreateDirectory(destDirName);
+                    File.SetAttributes(destDirName, File.GetAttributes(sourceDirName));
+                }
+
+                if (destDirName[destDirName.Length - 1] != System.IO.Path.DirectorySeparatorChar)
+                {
+                    destDirName = destDirName + System.IO.Path.DirectorySeparatorChar;
+                }
+
+                string[] files = Directory.GetFiles(sourceDirName);
+
+                if (logger && files != null && files.Length > 0)
+                {
+                    Console.WriteLine("--> " + sourceDirName);
+                }
+
+                //替换文件
+                foreach (string file in files)
+                {
+                    //排除更新使用的dll
+                    FileInfo info = new FileInfo(file);
+                    if (info.Name.ToLower().Equals(("ICSharpCode.SharpZipLib.dll").ToLower()) ||
+                        info.Name.ToLower().Equals(("TaskScheduler.dll").ToLower()) ||
+                        info.Name.ToLower().IndexOf(("Newtonsoft.Json.dll").ToLower()) >= 0 ||
+                        info.Name.ToLower().Equals(("d3dcompiler_47.dll").ToLower()) ||
+                        info.Name.ToLower().Equals(("New_SoftwareUpdater.exe").ToLower()) ||
+                        //info.Name.ToLower().Equals(("SoftwareAutoUpdater.exe").ToLower())||
+                        info.Name.Contains((".vshost")) || info.Name.Contains((".pdb"))
+                        )
+                    {
+                        continue;
+                    }
+                    //替换文件
+                    filename = file;
+                    File.Copy(file, destDirName + System.IO.Path.GetFileName(file), true);
+                    File.SetAttributes(destDirName + System.IO.Path.GetFileName(file), FileAttributes.Normal);
+                    if (logger)
+                    {
+                        Console.WriteLine("    -> " + System.IO.Path.GetFileName(file));
+                    }
+                    if (file.EndsWith("sql") && sqlScriptList != null)
+                    {
+                        sqlScriptList.Add(destDirName + System.IO.Path.GetFileName(file));
+                    }
+                }
+
+                //替换文件夹
+                string[] dirs = Directory.GetDirectories(sourceDirName);
+                foreach (string dir in dirs)
+                {
+                    //排除不备份的文件夹
+                    DirectoryInfo info = new DirectoryInfo(dir);
+                    if (info.Name.Equals("update") ||
+                        info.Name.Equals("log") ||
+                        info.Name.Equals("previousVersion") ||
+                        info.Name.Equals("database") ||
+                        info.Name.Equals("jre1.8") ||
+                        string.IsNullOrEmpty(dir))
+                    {
+                        continue;
+                    }
+
+                    Copy(dir, destDirName + System.IO.Path.GetFileName(dir), out error, sqlScriptList, logger);
+                }
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("替换文件失败: " + filename);
+                error = ex.Message + "文件:" + filename;
+                return false;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections;
+using System.Net;
+using System.Net.Sockets;
+
+namespace CSharpConsoleDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        
+
+            
+        }
+
+        /// <summary>
+        /// 模拟POS更新文件复制
+        /// </summary>
+        private static void copyFiles() {
+
+            string srcPath = "D:\\pack_new\\setup\\part_update_dir";
+            string dstPath = "D:\\tmp";
+            string outError = null;
+            ArrayList sqlScriptList = new ArrayList();
+            DirectoryCopy.Copy(srcPath, dstPath, out outError, sqlScriptList, true);
+            if (!string.IsNullOrEmpty(outError))
+            {
+                Console.WriteLine("error = " + outError);
+            }
+            else
+            {
+                if (sqlScriptList.Count > 0)
+                {
+                    Console.WriteLine("success sqlScript = " + (string)sqlScriptList[0]);
+                }
+            }
+            Console.WriteLine("--------------------");
+            return;
+        }
+
+
+        //文件是否存在
+        protected static void fileExists()
+        {
+            Console.WriteLine("Hello World!");
+            string Fi = @"D:\idea\hydee\pos-client\Bin\log\POS_2020-07-302.Log";
+            //这里可能是影藏文件
+            if (System.IO.File.Exists(@"D:\idea\hydee\pos-client\Bin\log\POS_2020-07-30.Log"))
+            {
+                Console.WriteLine("存在!");
+            }
+            else
+            {
+                Console.WriteLine("不存在!");
+            }
+        }
+
+        //检查端口是否
+        protected static void checkPortNum()
+        {
+            if (checkPortNum(1088))
+            {
+                Console.WriteLine("1088端口已占用!");
+            }
+            else
+            {
+                Console.WriteLine("1088端口未占用!");
+            }
+        }
+
+        protected static bool checkPortNum(int portNum)
+        {
+            /// 将IP和端口替换成为你要检测的
+            string ipAddress = "127.0.0.1";
+            IPAddress ip = IPAddress.Parse(ipAddress);
+            try
+            {
+                IPEndPoint point = new IPEndPoint(ip, portNum);
+                using (Socket sock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
+                {
+                    sock.Connect(point);
+                    sock.Close();
+                    return true;
+                }
+            }
+            catch (SocketException e)
+            {
+            }
+            return false;
+        }
+    }
+}
